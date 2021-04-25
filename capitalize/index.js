9fb9554b2e5a9fcc55613bd083350107111dbb06
zerogpm <zerogpm@gmail.com>
2021-04-25 08:42:03 -0700

    capitalize add second solution

--- capitalize/index.js
+++ capitalize/index.js
@@ -8,11 +8,23 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-    let words = [];
-    for (let word of str.split(" ")) {
-        words.push(word[0].toUpperCase() + word.slice(1));
+    // solution 1;
+    // let words = [];
+    // for (let word of str.split(" ")) {
+    //     words.push(word[0].toUpperCase() + word.slice(1));
+    // }
+    // return words.join(" ");
+
+    let result = str[0].toUpperCase();
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === " ") {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
     }
-    return words.join(" ");
+
+    return result;
 }
 
 module.exports = capitalize;
